fc16cdd4b6896c39932d5861ee01abd9439842f4
sameerkasi200x <sameer.kasi200x@gmail.com>
2017-09-04 01:52:16 +0800

    modified to accommodated a board of n*n
    - Working towards making this a generic game of any complexity. 
    - Modified the display_board function to display using a 2 dimensional list
    - Added win_check function - which works using a generic list
    - Introduced a global variable degree_of_board
    
    Todo:
    - Need to add checks to avoid the code from breaking when an invalid board is entered

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -1,18 +1,19 @@
 from __future__ import print_function
 from IPython.display import clear_output
+from IPython.display import clear_output
+
+degree_of_board=3
+
 def display_board(board):
     clear_output()
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
+    x=1
+    while x<degree_of_board+1 :
+        print('   |   |')
+        print(' ' + board[x][1] + ' | ' + board[x][2] + ' | ' + board[x][3])
+        print('   |   |')
+        if not (x==3):
+            print('-----------')
+        x=x+1;
     
 def player_input():
     player_markers={'p1':'','p2':''}
@@ -26,3 +27,74 @@ def player_input():
     else:
         player_markers=player_input()
     return player_markers
+
+def win_check(board,mark):
+    ## Check horizontal
+    x=1;
+    y=1;
+    
+    
+    win_status=False
+    while x<degree_of_board+1 :
+        while y<degree_of_board+1 :
+        # Perform check for for all markers in a given X-axis line
+            if not (board[x][y]==mark) :
+                win_status=False;
+                break
+            else :
+                y=y+1
+                win_status=True
+        
+        ## If the win status remains true by the end of the given X-axis line, player with given marker has won
+        if win_status :
+            return win_status
+        else: 
+            x=x+1
+            y=1
+
+    ## Check vertical
+    x=1;
+    y=1;
+    win_status=False
+    while y<degree_of_board+1 :
+        while x<degree_of_board+1 :
+            # Perform check for all markers in given Y-axis
+            if not (board[x][y]==mark) :
+                win_status=False;
+                break
+            else :
+                x=x+1
+                win_status=True
+        
+        ## If the win status remains true by the end of the given Y-axis line, player with given marker has won
+        if win_status :
+            return win_status
+        else: 
+            y=y+1
+            x=1
+    
+    ## Check diagonal Right-top to bottom-left
+    x=1;
+    while x<degree_of_board+1 :
+        if not (board[x][x]==mark) :
+            win_status=False
+            break
+        else :
+            x=x+1
+            win_status=True
+    if win_status :
+            return win_status
+
+    ## Check diagonal bottom-left to top-right
+    x=1;
+    while x<degree_of_board+1 :
+        if not (board[x][degree_of_board+1-x]==mark) :
+            win_status=False
+            break
+        else :
+            x=x+1
+            win_status=True
+    if win_status :
+            return win_status
+        
+    return False
